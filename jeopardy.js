34d67cb64c871d8570ebda6da1f7c5c7906f7d22
Carl Mesias <carlmesias20@gmail.com>
2024-10-09 19:37:34 -0700

    Add listening for clicks on clues
    When clicking on a clue, we are able to console log the id value (event.target) of the clue. We need this value to append it to #active-clue in the next iteration

--- jeopardy.js
+++ jeopardy.js
@@ -49,6 +49,7 @@ Category: The name given to the structure containing clues on the same topic.
 }
  */
 
+// document.addEventListener('DOMContentLoaded', function () {
 const API_URL = "https://rithm-jeopardy.herokuapp.com/api/"; // The URL of the API.
 const NUMBER_OF_CATEGORIES = 5; // The number of categories you will be fetching. You can change this number.
 const NUMBER_OF_CLUES_PER_CATEGORY = 5; // The number of clues you will be displaying per category. You can change this number.
@@ -93,100 +94,18 @@ $("#play").on("click", handleClickOfPlay);
  * - Sets up the game when the play button is clickable.
  */
 function handleClickOfPlay() {
-  // todo set the game up if the play button is clickable
-  AppendClueToCategryColumn();
+  if (isPlayButtonClickable) {
+    isPlayButtonClickable = false;
+
+    // Create 5 rows under table heads
+    Create5Rows();
+
+    // todo set the game up if the play button is clickable
+    AppendClueToCategryColumn();
+  }
 }
 
-/**
- * Sets up the game.
- *
- * 1. Cleans the game since the user can be restarting the game.
- * 2. Get category IDs
- * 3. For each category ID, get the category with clues.
- * 4. Fill the HTML table with the game data.
- *
- * Hints:
- * - The game play is managed via events.
- */
-async function setupTheGame() {
-  // todo show the spinner while setting up the game
-
-  // todo reset the DOM (table, button text, the end text)
-
-  // todo fetch the game data (categories with clues)
-
-  // todo fill the table
-}
-
-/**
- * Gets as many category IDs as in the `NUMBER_OF_CATEGORIES` constant.
- * Returns an array of numbers where each number is a category ID.
- *
- * Hints:
- * TODO [x] - Use /categories endpoint of the API.
- * TODO [x] - Request as many categories as possible, such as 100.
- * TODO [x] - Randomly pick as many categories as given in the `NUMBER_OF_CATEGORIES` constant
- * TODO [x] - if the number of clues in the category is enough (<= `NUMBER_OF_CLUES_PER_CATEGORY` constant).
- * 
- */
-async function getCategoryIds() {
-  // todo fetch NUMBER_OF_CATEGORIES amount of categories
-  const response = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/categories/?count=${NUMBER_OF_CATEGORIES}`)
-
-  // todo set after fetching
-  let ids = [];
-
-  // Loop through response data
-  response.data.forEach(function (value) {
-    if (value.clues_count <= NUMBER_OF_CLUES_PER_CATEGORY) {
-      ids.push(value.id)
-    }
-  });
-
-  return ids;
-}
-
-/**
- * Gets category with as many clues as given in the `NUMBER_OF_CLUES_PER_CATEGORY` constant.
- * Returns the below data structure:
- *  {
- *    "id": <category ID>
- *    "title": <category name> // baseball
- *    "clues": [
- *      {
- *        "id": <clue ID>,
- *        "value": <value of the question>,
- *        "question": <question>,
- *        "answer": <answer to the question>
- *      },
- *      ... more clues
- *    ]
- *  }
- *
- * Hints:
- * TODO [x] - You need to call this function for each category ID returned from the `getCategoryIds` function.
- * TODO [x] - Use /category endpoint of the API.
- * TODO [ ] - In the API, not all clues have a value. You can assign your own value or skip that clue.
- */
-async function getCategoryData(categoryId) {
-
-  const categoryWithClues = {
-    id: categoryId,
-    title: undefined, // TODO [x] - todo set after fetching
-    clues: []         // TODO [x] - todo set after fetching
-  };
-
-  // todo fetch the category with NUMBER_OF_CLUES_PER_CATEGORY amount of clues
-  let res = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/category?id=${categoryId}`)
-
-  // This is a category we fetched with it's id, store it in 'categories' array
-  categoryWithClues.title = res.data.title;
-  categoryWithClues.clues = res.data.clues;
-
-  return categoryWithClues;
-}
-
-function AppendClueToCategryColumn() {
+function Create5Rows() {
 
   ///////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////
@@ -208,12 +127,101 @@ function AppendClueToCategryColumn() {
   // Create amount of rows from NUMBER_OF_CATEGORIES variable
   ///////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////
+}
+
+/**
+ * Sets up the game.
+ *
+ * 1. Cleans the game since the user can be restarting the game.
+ * 2. Get category IDs
+ * 3. For each category ID, get the category with clues.
+ * 4. Fill the HTML table with the game data.
+ *
+ * Hints:
+ * - The game play is managed via events.
+ */
+async function setupTheGame() {
+  // [ ] - todo show the spinner while setting up the game
+
+  // [ ] - todo reset the DOM (table, button text, the end text)
+
+  // [ ] - todo fetch the game data (categories with clues)
+
+  // [ ] - todo fill the table
+}
+
+/**
+ * Gets as many category IDs as in the `NUMBER_OF_CATEGORIES` constant.
+ * Returns an array of numbers where each number is a category ID.
+ *
+ * Hints:
+ * - Use /categories endpoint of the API.
+ * - Request as many categories as possible, such as 100.
+ * - Randomly pick as many categories as given in the `NUMBER_OF_CATEGORIES` constant
+ * - if the number of clues in the category is enough (<= `NUMBER_OF_CLUES_PER_CATEGORY` constant).
+ * 
+ */
+async function getCategoryIds() {
+  // todo fetch NUMBER_OF_CATEGORIES amount of categories
+  const response = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/categories/?count=${NUMBER_OF_CATEGORIES}`)
+
+  // todo set after fetching
+  let ids = [];
+
+  // Store 'response.data' in 'data'
+  let data = response.data;
+
+  // Loop through response data
+  data.forEach(function (value) {
+    if (value.clues_count <= NUMBER_OF_CLUES_PER_CATEGORY) {
+      ids.push(value.id)
+    }
+  });
+
+  return ids;
+}
+
+// Gets category with as many clues as given in the `NUMBER_OF_CLUES_PER_CATEGORY` constant.
+async function getCategoryData(categoryId) {
+
+  /**
+   * Gets category with as many clues as given in the `NUMBER_OF_CLUES_PER_CATEGORY` constant.
+   * Returns the below data structure:
+   *  {
+   *    "id": <category ID>
+   *    "title": <category name> // baseball
+   *    "clues": [
+   *      {
+   *        "id": <clue ID>,
+   *        "value": <value of the question>,
+   *        "question": <question>,
+   *        "answer": <answer to the question>
+   *      },
+   *      ... more clues
+   *    ]
+   *  }
+   *
+   */
+  const categoryWithClues = {
+    id: categoryId,
+    title: undefined, // TODO [x] - todo set after fetching
+    clues: []         // TODO [x] - todo set after fetching
+  };
+
+  // todo fetch the category with NUMBER_OF_CLUES_PER_CATEGORY amount of clues
+  let res = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/category?id=${categoryId}`)
+
+  // This is a category we fetched with it's id, store it in 'categories' array
+  categoryWithClues.title = res.data.title;
+  categoryWithClues.clues = res.data.clues;
+
+  return categoryWithClues;
+}
+
+async function AppendClueToCategryColumn() {
 
   // Loop through each category id from our promise
-  let currentCategoryIdx = 0; // goes from left to right, 0-4 (inclusive)
-  let afterColumnIdx = 0; // goes from left to right, 0-4 (inclusive)
-
-  getCategoryIds().then((arr) => {
+  await getCategoryIds().then((arr) => {
 
     // parse array of ids, need to access each element in the array to get the category id
     arr.forEach(function (id) {
@@ -246,22 +254,21 @@ function AppendClueToCategryColumn() {
         const row_3 = document.getElementById('row_3');  // 'row_0'
         const row_4 = document.getElementById('row_4');  // 'row_0'
 
-        ///////////////////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
         // Insert each clue from each category at the end of each row
 
         // Create clues arr for this category
         let clueIdx = 0;
 
-        // Loop through each clue, then push it to 'clues' array
+        // Loop through each clue, then append to corresponding column
         for (let clue of category.clues) {
           // Create td to store clue value
           const td = document.createElement('td');
           td.innerText = `$${clue.value}`;  // "$100", "$500" /// td.innerText = clue.question;
 
-          // Store category.id in class of a clue
-          // td.classList.add(`category_${category.id}`);
-          td.classList.add(`clue`);
+          // Add class 'clue' to 'td'
+          td.classList.add(`clues`);
 
           // Store clue.id in clue id
           td.setAttribute('id', clue.id)
@@ -285,6 +292,7 @@ function AppendClueToCategryColumn() {
           else if (clueIdx == 4) {
             row_4.append(td);
           }
+
           /// Loop through each clue array then append at the end of each row ///
           ///////////////////////////////////////////////////////////////////////
 
@@ -292,96 +300,91 @@ function AppendClueToCategryColumn() {
           clueIdx++;
 
           // Insert each clue from each category at the end of each row
-          ///////////////////////////////////////////////////////////////////////
-          ///////////////////////////////////////////////////////////////////////
+          /////////////////////////////////////////////////////////////////////////////////////
+          /////////////////////////////////////////////////////////////////////////////////////
         }
+
       });
       // After above function completes do this
     });
   });
-}
 
-/**
- * Fills the HTML table using category data.
- *
- * Hints:
- * - You need to call this function using an array of categories where 
- *   each element comes from the `getCategoryData` function.
- * 
- * - Table head (thead) has a row (#categories).
- *   For each category, you should create a cell element (th) and append that to it.
- * 
- * - Table body (tbody) has a row (#clues).
- *   For each category, you should create a cell element (td) and append that to it.
- * 
- *   Besides, for each clue in a category, you should create a row element (tr) 
- *   and append it to the corresponding previously created and appended cell element (td).
- * 
- * - TODO [ ] - To this row elements (tr) should add an event listener (handled by the `handleClickOfClue` function) 
- *   and set their IDs with category and clue IDs. This will enable you to detect which clue is clicked.
- */
-function fillTable(categories) {
-  // todo
-}
+  /**
+    * - TODO [ ] - To this row elements (tr) should add an event listener (handled by the `handleClickOfClue` function) 
+    *   and set their IDs with category and clue IDs. This will enable you to detect which clue is clicked.
+   */
 
-$(".clue").on("click", handleClickOfClue);
+  // Listen for clicks on clues
+  $('table').on('click', '.clues', function (event) {
 
-/**
- * Manages the behavior when a clue is clicked.
- * Displays the question if there is no active question.
- *
- * Hints:
- * - Control the behavior using the `activeClueMode` variable.
- * - Identify the category and clue IDs using the clicked element's ID.
- * - Remove the clicked clue from categories since each clue should be clickable only once. Don't forget to remove the category if all the clues are removed.
- * - Don't forget to update the `activeClueMode` variable.
- *
- */
-function handleClickOfClue(event) {
-  
-  // TODO [ ] - show clue question when being clicked on
-  // const clues = document.querySelectorAll('.clue');
-  // console.log('clues: ', clues);
+    // Represents the parent when clicking on the parent and when clicking inside its child
+    // const currentTarget = event.currentTarget.getAttribute("id");
 
-  // clues.addEvenetListener('click', function(clue) {
+    // Usually the child inside a parent
+    const target = event.target.getAttribute("id");
 
-  //   console.log('clue: ', clue);
+    handleClickOfClue(`value: ${value}`)
   });
-  // todo find and remove the clue from the categories
 
-  // todo mark clue as viewed (you can use the class in style.css), display the question at #active-clue
-}
+  /**
+   * Manages the behavior when a clue is clicked.
+   * Displays the question if there is no active question.
+   *
+   * Hints:
+   * - Control the behavior using the `activeClueMode` variable.
+   * - Identify the category and clue IDs using the clicked element's ID.
+   * - Remove the clicked clue from categories since each clue should be clickable only once. Don't forget to remove the category if all the clues are removed.
+   * - Don't forget to update the `activeClueMode` variable.
+   *
+   */
+  function handleClickOfClue(event) {
+    console.log('event ', event);
 
-$("#active-clue").on("click", handleClickOfActiveClue);
+    // show clue question when being clicked on
+    // const clues = document.querySelectorAll('.clue');
+    // console.log('clues: ', clues);
 
-/**
- * Manages the behavior when a displayed question or answer is clicked.
- * Displays the answer if currently displaying a question.
- * Clears if currently displaying an answer.
- *
- * Hints:
- * - Control the behavior using the `activeClueMode` variable.
- * - After clearing, check the categories array to see if it is empty to decide to end the game.
- * - Don't forget to update the `activeClueMode` variable.
- */
-function handleClickOfActiveClue(event) {
-  // todo display answer if displaying a question
+    // clues.addEvenetListener('click', function(clue) {
 
-  // todo clear if displaying an answer
-  // todo after clear end the game when no clues are left
+    //   console.log('clue: ', clue);
+    //});
+    // todo find and remove the clue from the categories
 
-  if (activeClueMode === 1) {
-    activeClueMode = 2;
-    $("#active-clue").html(activeClue.answer);
+    // todo mark clue as viewed (you can use the class in style.css), display the question at #active-clue
   }
-  else if (activeClueMode === 2) {
-    activeClueMode = 0;
-    $("#active-clue").html(null);
 
-    if (categories.length === 0) {
-      isPlayButtonClickable = true;
-      $("#play").text("Restart the Game!");
-      $("#active-clue").html("The End!");
+  $("#active-clue").on("click", handleClickOfActiveClue);
+
+  /**
+   * Manages the behavior when a displayed question or answer is clicked.
+   * Displays the answer if currently displaying a question.
+   * Clears if currently displaying an answer.
+   *
+   * Hints:
+   * - Control the behavior using the `activeClueMode` variable.
+   * - After clearing, check the categories array to see if it is empty to decide to end the game.
+   * - Don't forget to update the `activeClueMode` variable.
+   */
+  function handleClickOfActiveClue(event) {
+    // todo display answer if displaying a question
+
+    // todo clear if displaying an answer
+    // todo after clear end the game when no clues are left
+
+    if (activeClueMode === 1) {
+      activeClueMode = 2;
+      $("#active-clue").html(activeClue.answer);
+    }
+    else if (activeClueMode === 2) {
+      activeClueMode = 0;
+      $("#active-clue").html(null);
+
+      if (categories.length === 0) {
+        isPlayButtonClickable = true;
+        $("#play").text("Restart the Game!");
+        $("#active-clue").html("The End!");
+      }
     }
   }
 }
+// });
